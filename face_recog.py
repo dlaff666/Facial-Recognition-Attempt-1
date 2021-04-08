89740432023ec277cd0bc6a5c479511871de317f
dlaff666 <70662517+dlaff666@users.noreply.github.com>
2021-04-08 11:31:53 -0400

    Got it working

--- face_recog.py
+++ face_recog.py
@@ -4,38 +4,39 @@ import pickle
 import cv2
 import os
  
-#get paths of each file in folder named Images
-#Images here contains my data(folders of various persons)
-imagePaths = list(paths.list_images('Images'))
-knownEncodings = []
-knownNames = []
+def buildDatabse():
+    #get paths of each file in folder named Images
+    imagePaths = list(paths.list_images('Images'))
+    knownEncodings = []
+    knownNames = []
 
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
+    # loop over the image paths
+    for (i, imagePath) in enumerate(imagePaths):
 
-    # extract the person name from the image path
-    name = imagePath.split(os.path.sep)[-2]
+        # extract the person name from the image path
+        name = imagePath.split(os.path.sep)[-2]
 
-    # load the input image and convert it from BGR (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # load the input image and convert it from BGR (OpenCV ordering) to dlib ordering (RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    #Use Face_recognition to locate faces
-    boxes = face_recognition.face_locations(rgb,model='hog')
+        #Use Face_recognition to locate faces
+        boxes = face_recognition.face_locations(rgb,model='hog')
 
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
+        # compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
 
-    # loop over the encodings
-    for encoding in encodings:
-        knownEncodings.append(encoding)
-        knownNames.append(name)
+        # loop over the encodings
+        for encoding in encodings:
+            knownEncodings.append(encoding)
+            knownNames.append(name)
 
-#save emcodings along with their names in dictionary data
-data = {"encodings": knownEncodings, "names": knownNames}
+    #save emcodings along with their names in dictionary data
+    data = {"encodings": knownEncodings, "names": knownNames}
 
-#use pickle to save data into a file for later use
-f = open("face_enc", "wb")
-f.write(pickle.dumps(data))
-f.close()
+    #use pickle to save data into a file for later use
+    f = open("face_enc", "wb")
+    f.write(pickle.dumps(data))
+    f.close()
+    
+buildDatabse()
